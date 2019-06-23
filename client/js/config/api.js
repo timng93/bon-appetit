537b9d0774d405f9f4c32ca216f3e3079dd8cec8
Timmy <timgabrielnguyen@gmail.com>
2019-06-23 16:41:38 -0700

    Init Apollo Client

--- client/js/config/api.js
+++ client/js/config/api.js
@@ -1,1 +1,7 @@
-// TODO: Add apollo dependencies
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "https://api.graph.cool/simple/v1/cjx3y7yoy4t9501347awq4o35"
+});
+
+export default client;
