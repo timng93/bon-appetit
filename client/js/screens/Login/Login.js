532ba0751775eecf964d3e3d48c7c85edcdbe4fa
Timmy <timgabrielnguyen@gmail.com>
2019-06-25 15:44:43 -0700

    Debug Login Screen

--- client/js/screens/Login/Login.js
+++ client/js/screens/Login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {View, Text, Button} from "react-native";
 import AsyncStorage from "@react-native-community/async-storage";
-import {graphql} from "react-apollo";
+import {graphql, compose} from "react-apollo";
 import gql from "graphql-tag";
 import PropTypes from "prop-types";
 import styles from "./styles";
